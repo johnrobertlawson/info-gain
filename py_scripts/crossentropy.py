7af202198e99060a1a8c51c457818e956edeb091
John Lawson <johnroblawson@gmail.com>
2022-05-09 14:40:56 -0500

    Moved back to ipynb but it's broken.

--- py_scripts/crossentropy.py
+++ py_scripts/crossentropy.py
@@ -1,3 +1,13 @@
+"""XES score.
+"""
+import os
+import pdb
+
+import numpy as np
+
+from scipy.stats import gmean as geomean
+
+
 class CrossEntropy:
     """ For computing and visualising skill scores for a probabilistic forecast.
     Currently for two categories (binary) only, but with observational error.
@@ -10,23 +20,29 @@ class CrossEntropy:
         o:  1-D obs forecast [0,1], may or may not include obs uncertainty
         fk: 1-D array of probability levels. If None, compute automatically.
         """
-        self.f = f
-        self.o = o
+        self.f = np.array(f)
+        self.o = np.array(o)
         self.logbase = 2
+        # self.use_mean = "geometric"
+        self.use_mean = "arithmetic"
 
         if fk == None:
             self.fk = np.unique(self.f)
         else:
-            self.fk = fk
+            raise Exception
+            # NOT IMPLEMENTED
+            # self.fk = fk
 
-    @staticmethod
-    def do_geomean(n):
-        # if isinstance(n,(np.float,float,np.int,int)):
-        # pdb.set_trace()
+    def do_mean(self,n):
+        if self.use_mean == "geometric":
+            f = geomean
+        elif self.use_mean == "arithmetic":
+            f = np.mean
+
         if isinstance(n,(float,int)):
             return n
         else:
-            return geomean(n)
+            return f(n)
 
     @staticmethod
     def compute_DKL(x,y,return_all=False):
@@ -36,23 +52,29 @@ class CrossEntropy:
         x   : 1-D (e.g., observations)
         y   : 1-D (e.g., forecasts)
         """
+        if not isinstance(x,np.ndarray):
+            x = np.array([x,])
+        if not isinstance(y,np.ndarray):
+            y = np.array([y,])
         with np.errstate(divide='ignore',invalid='ignore'):
             # Enforce array so we can remove nans
-            term1 = np.array(((1-x) * np.log2((1-x)/(1-y))))
-            term2 = np.array((x * np.log2(x/y)))
+            term1 = (1-x) * np.log2((1-x)/(1-y))
+            term2 = x * np.log2(x/y)
 
         term1[x==1] = 0
         term2[x==0] = 0
 
         DKL_all = term1 + term2
         DKL = np.mean(DKL_all)
+        # print(f"DKL = {DKL:.4f}")
+
         if return_all:
             return DKL, DKL_all
         return DKL
 
-    def compute_XESS(self,with_H=True,from_components=False):
+    def compute_XESS(self,from_components=False):
         UNC = self.compute_UNC()
-        XES = self.compute_XES(with_H=with_H)
+        XES = self.compute_XES(from_components=from_components)
         XESS = (XES-UNC)/(0-UNC)
         return XESS
 
@@ -60,104 +82,77 @@ class CrossEntropy:
     def compute_entropy(x,return_all=False):
         """ Average surprise. A series of surprise
         values are returned if return_all is True.
-
-        Is this right? Surprise v UNC?
-
         """
-        H_all = (-(1-x) * np.log2(1-x)) + -(x * np.log2(x))
-        # H_all = ((1-x) * np.log2(1-x)) - (x * np.log2(x))
+        with np.errstate(divide='ignore',invalid='ignore'):
+            H_all = (-(1-x) * np.log2(1-x)) - (x * np.log2(x))
         H = np.mean(H_all)
         if return_all:
             return H, H_all
         return H
 
-    def compute_H(self):
-        """Return entropy of the observation field.
-        """
-        return self.compute_entropy(self.o)
-
-    def compute_XES(self,with_H=True,from_components=False,return_all=False):
+    def compute_XES(self,from_components=False,return_all=False,use_dkl=False):
         """ Cross-entropy score.
 
         Args:
 
         """
-        if with_H:
-            if return_all:
-                H, H_all = self.compute_H(return_all=True)
-            else:
-                H = self.compute_H()
-
         if from_components:
             REL = self.compute_REL()
             DSC = self.compute_DSC()
             UNC = self.compute_UNC()
-            if with_H:
-                XES = REL-DSC+UNC+H
-            else:
-                XES = REL-DSC+UNC
-        else:
+            XES = REL-DSC+UNC
+        elif use_dkl:
             if return_all:
                 XES, XES_all = self.compute_DKL(self.o,self.f,return_all=True)
             else:
                 XES = self.compute_DKL(self.o,self.f)
+        else:
+            with np.errstate(divide='ignore',invalid='ignore'):
+                XES = np.mean(-((1-self.o)*np.log2(1-self.f))-
+                        (self.o*np.log2(self.f)))
 
-            if with_H:
-                XES += H
-                if return_all:
-                    XES_all += H_all
-                    return XES, XES_all
-
+        if return_all:
+            return XES, XES_all
         return XES
-
-    @staticmethod
-    def single_XES(f,o):
-        """No observational error!
-        """
-        return ((1-o)*np.log2((1-o)/(1-f)))+(o*np.log2(o/f))
-
-    @classmethod
-    def single_XESS(cls,f,o):
-        """No observational error!
-        """
-        XES = cls.single_XES(f,o)
-        XES_UNC = cls.single_UNC(f,o)
-        XESS = (XES-XES_UNC)/(0-XES_UNC)
-        return XESS
-
-    @staticmethod
-    def single_UNC(f,o):
-        """No observational error!
-        """
-        return (-o*np.log2(o))-((1-o)*np.log2(1-o))
-
-    def compute_DSC(self):
-        dsc = np.zeros_like(self.fk)
-        for nk,k in enumerate(self.fk):
-            ok = self.o[self.f==k]
-            ok_hat = self.do_geomean(ok)
-            o_hat = self.do_geomean(self.o)
-            dsc[nk] = ok.size * self.compute_DKL(ok_hat,o_hat)
-        DSC = np.sum(dsc)/self.o.size
-        return DSC
 
     def compute_REL(self):
         rel = np.zeros_like(self.fk)
         for nk,k in enumerate(self.fk):
             ok = self.o[self.f==k]
-            ok_hat = self.do_geomean(ok)
-            rel[nk] = ok.size * self.compute_DKL(ok_hat,k)
+            if ok.size != 0:
+                ok_hat = self.do_mean(ok)
+                rel[nk] = ok.size * self.compute_DKL(ok_hat,k)
         REL = np.sum(rel)/self.o.size
+        print(f"{rel=}, REL = {REL:.4f}")
         return REL
 
-    def compute_UNC(self):
+    def compute_DSC(self):
+        dsc = np.zeros_like(self.fk)
+        for nk,k in enumerate(self.fk):
+            ok = self.o[self.f==k]
+            if ok.size != 0:
+                ok_hat = self.do_mean(ok)
+                o_hat = self.do_mean(self.o)
+                dsc[nk] = ok.size * self.compute_DKL(ok_hat,o_hat)
+        DSC = np.sum(dsc)/self.o.size
+        print(f"{dsc=}, DSC = {DSC:.4f}")
+        return DSC
+
+    def __compute_UNC(self):
         """ Compute uncertainty component of the forecast.
         """
-        p_o = self.do_geomean(self.o)
-        # with np.errstate(divide='ignore',invalid='ignore'):
-        term1 = -p_o*np.log2(p_o)
-        term2 = (1-p_o)*np.log2(1-p_o)
-        UNC = term1 - term2
+        p_o = self.do_mean(self.o)
+        with np.errstate(divide='ignore',invalid='ignore'):
+            term1 = -p_o*np.log2(p_o)
+            term2 = -(1-p_o)*np.log2(1-p_o)
+        UNC = term1 + term2
+        print(f"UNC = {UNC:.4f}")
+        return UNC
+
+    def compute_UNC(self):
+        p_o = self.do_mean(self.o)
+        UNC = self.compute_entropy(p_o)
+        print(f"UNC = {UNC:.4f}")
         return UNC
 
     @staticmethod
@@ -169,18 +164,3 @@ class CrossEntropy:
         x[x<thresh] = thresh
         x[x>(1-thresh)] = 1-thresh
         return x
-
-    def count_bin_size(x,fk):
-        """From Wilks: need to bin fcst probs, setting the middle of
-        the bin as the value...
-
-        """
-        bin_counts,bin_edges = np.histogram(x,bins=fk)
-        return bin_counts
-
-    def quantise(x,fk):
-        # I think the index is not right, need to look at bin edges?
-        quant_probs = np.digitize(x,fk)
-        # pdb.set_trace()
-        f = [fk[q-1] for q in quant_probs]
-        return f
--- py_scripts/do_tests.py
+++ py_scripts/do_tests.py
@@ -10,14 +10,21 @@ np.set_printoptions(precision=4,suppress=True)
 pd.set_option("display.precision",4)
 pd.set_option("display.float_format",'{:.4f}'.format)
 
-def print_tests(f,o,fk,title,ob_unc):
-    fk = None
-    brier = BrierScore(f,o,fk=fk,ob_uncertainty=ob_unc)
-
-    print(title,(" "*5),f"BS = {brier.compute_BS():.4f}",("="*20),)
-    print(f"BS from comp = {brier.compute_BS(from_components=True):.4f}\n")
-    # print("\n")
+do_brier = False
+do_xes = True
+def print_tests(f,o,title,ob_unc):
+    if do_brier:
+        brier = BrierScore(f,o,ob_uncertainty=ob_unc)
+        print(title,(" "*5),f"BS = {brier.compute_BS():.4f}",("="*20),)
+        print(f"BS from comp = {brier.compute_BS(from_components=True):.4f}\n")
+    if do_xes:
+        xes = CrossEntropy(f,o)
+        print(title,(" "*5),
+            f"XES = {xes.compute_XES(from_components=False):.4f}",
+                        ("="*20),)
+        print(f"XES from comp= {xes.compute_XES(from_components=True):.4f}\n")
     return
+
 
 # Testing for one forecast
 
@@ -25,8 +32,7 @@ def print_tests(f,o,fk,title,ob_unc):
 # Obs are binary
 f = [0.4,]
 o = [1,]
-fk = np.arange(0,1.01,0.1)
-print_tests(f,o,fk,"SINGLE:",False)
+print_tests(f,o,"SINGLE:",False)
 
 ###########################################################
 
@@ -36,40 +42,29 @@ print_tests(f,o,fk,"SINGLE:",False)
 # Obs are binary
 f = [0.1,0.5,0.9,0.8,0.7,0.2,1,0,0.1]
 o = [0,0,1,1,0,0,1,1,0]
-fk = np.arange(0,1.01,0.1)
-print_tests(f,o,fk,"RAW FALSE:",False)
+print_tests(f,o,"RAW:",False)
 
 # Probs are bounded to [0.01,0.99]
 # Obs are binary
 f = [0.1,0.5,0.9,0.8,0.7,0.2,0.99,0.01,0.1]
 o = [0,0,1,1,0,0,1,1,0]
-fk = [0.01,] + list(np.arange(0.1,0.91,0.1)) + [0.99,]
-print_tests(f,o,fk,"PROBS 1pc:",False)
-
-# Probs are bounded to [0.01,0.99]
-# Obs are binary
-f = [0.1,0.5,0.9,0.8,0.7,0.2,0.99,0.01,0.1]
-o = [0,0,1,1,0,0,1,1,0]
-print_tests(f,o,None,"PROBS 1pc -- NO FK:",False)
+print_tests(f,o,"PROBS 1pc:",False)
 
 # Probs are bounded to [0.01,0.99]
 # Obs are from {0.01,0.99} (same certainty)
 f = [0.1,0.5,0.9,0.8,0.7,0.2,0.99,0.01,0.1]
 o = [0.01,0.01,0.99,0.99,0.01,0.01,0.99,0.99,0.01]
-fk = [0.01,] + list(np.arange(0.1,0.91,0.1)) + [0.99,]
-print_tests(f,o,fk,"BOTH 1pc:",True)
+print_tests(f,o,"BOTH 1pc FALSE:",False)
 
 # Probs are bounded to [0.01,0.99]
 # Obs are from {0.001,0.999} (high certainty)
 f = [0.1,0.5,0.9,0.8,0.7,0.2,0.99,0.01,0.1]
 o = [0.001,0.001,0.999,0.999,0.001,0.001,0.999,0.999,0.001]
-fk = [0.01,] + list(np.arange(0.1,0.91,0.1)) + [0.99,]
-print_tests(f,o,fk,"OBS 0.1pc:",True)
+print_tests(f,o,"OBS 0.1pc:",True)
 
 # Testing for multiple forecasts
 # Probs are bounded to [0.01,0.99]
 # Obs are from {0,0.999} (ignore null forecast error)
 f = [0.1,0.5,0.9,0.8,0.7,0.2,0.99,0.01,0.1]
 o = [0,0,0.999,0.999,0,0,0.999,0.999,0]
-fk = [0.01,] + list(np.arange(0.1,0.91,0.1)) + [0.99,]
-print_tests(f,o,fk,"IGNORE NULL",True)
+print_tests(f,o,"IGNORE NULL",True)
